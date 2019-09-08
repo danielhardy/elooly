0a9750f78203554a4eda33faac3115859c2f3155
Ubuntu <ubuntu@ip-10-0-0-20.ec2.internal>
2019-09-08 17:13:56 +0000

    Changed SERVER name to Chit Chatter

--- chatterbox.js
+++ chatterbox.js
@@ -55,9 +55,9 @@ io.sockets.on('connection', function (socket) {
 		// send client to room 1
 		socket.join('room1');
 		// echo to client they've connected
-		socket.emit('updatechat', 'SERVER', 'you have connected to a new room');
+		socket.emit('updatechat', 'Chit Chatter[server]', 'you have connected to a new room');
 		// echo to room 1 that a person has connected to their room
-		socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
+		socket.broadcast.to('room1').emit('updatechat', 'Chit Chatter[server]', username + ' has connected to this room');
 		socket.emit('updaterooms', rooms, 'room1');
 	});
 
@@ -70,12 +70,12 @@ io.sockets.on('connection', function (socket) {
 	socket.on('switchRoom', function(newroom){
 		socket.leave(socket.room);
 		socket.join(newroom);
-		socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+		socket.emit('updatechat', 'Chit Chatter[server]', 'you have connected to '+ newroom);
 		// sent message to OLD room
-		socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
+		socket.broadcast.to(socket.room).emit('updatechat', 'Chit Chatter[server]', socket.username+' has left this room');
 		// update socket session room title
 		socket.room = newroom;
-		socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
+		socket.broadcast.to(newroom).emit('updatechat', 'Chit Chatter[server]', socket.username+' has joined this room');
 		socket.emit('updaterooms', rooms, newroom);
 	});
 
@@ -87,7 +87,7 @@ io.sockets.on('connection', function (socket) {
 		// update list of users in chat, client-side
 		io.sockets.emit('updateusers', usernames);
 		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+		socket.broadcast.emit('updatechat', 'Chit Chatter[server]', socket.username + ' has disconnected');
 		socket.leave(socket.room);
 	});
 });
